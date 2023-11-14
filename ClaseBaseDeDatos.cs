1aaebb7ef0377310d134aa4332859b16ed296c18
Alumnos <Alumnos@PC187>
2023-11-14 14:30:44 -0300

    Agregar archivos de proyecto.

---
+++ ClaseBaseDeDatos.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.OleDb;
+using System.Windows.Forms;
+using System.Data;
+using System.Reflection.Emit;
+
+
+
+namespace PryTpLab2
+{
+    internal class ClaseBaseDeDatos
+    {
+        //Objetos
+        OleDbConnection conexionBD;
+        OleDbCommand comandoBD;
+        OleDbDataReader lectorBD;
+        OleDbDataAdapter adaptadorBD;
+        DataSet objDataSet = new DataSet();
+
+        string cadenaConexion = @"Provider = Microsoft.ACE.OLEDB.12.0;" + " Data Source = ..\\..\\Bin\\Debug\\VERDULEROS";
+
+        public string estadoConexion = "";
+        public string datosTabla;
+
+        public void ConectarBD()
+        {
+            try
+            {
+                conexionBD = new OleDbConnection();
+                conexionBD.ConnectionString = cadenaConexion;
+                conexionBD.Open();
+                estadoConexion = "Conectado";
+            }
+            catch (Exception ex)
+            {
+                estadoConexion = "Error" + ex.Message;
+            }
+        }
+        public void TraerDatos(DataGridView grilla)
+        {
+            //instancia un objeto en la memoria
+            comandoBD = new OleDbCommand();
+
+            //conecta el comando con la conexion
+            comandoBD.Connection = conexionBD;
+            comandoBD.CommandType = System.Data.CommandType.TableDirect;
+            comandoBD.CommandText = "Productos";
+
+            lectorBD = comandoBD.ExecuteReader();
+            grilla.Columns.Add("Codigo del Producto", "Codigo del Producto");
+            grilla.Columns.Add("NomProducto", "NomProducto");
+            grilla.Columns.Add("IDGrupo", "IDGrupo");
+            grilla.Columns.Add("Precio", "Precio");
+            
+
+            if (lectorBD.HasRows)
+            {
+                while (lectorBD.Read())
+                {
+
+                    datosTabla += "-" + lectorBD[1];
+                    grilla.Rows.Add(lectorBD[0], lectorBD[1], lectorBD[2], lectorBD[4], lectorBD[5], lectorBD[6], lectorBD[7]);
+                }
+            }
+        }
+
+        public void BuscarPorCodigo(int codigo)
+        {
+            comandoBD = new OleDbCommand();
+
+            comandoBD.Connection = conexionBD;
+            comandoBD.CommandType = System.Data.CommandType.TableDirect;
+            //Que tipo de operacion quier hacer y que me traiga toda la tabla con el tabledirect
+            comandoBD.CommandText = "Productos";
+            //Que tabla traigo
+
+            lectorBD = comandoBD.ExecuteReader();
+            //Abre la tabla y muestra por renglón
+
+            if (lectorBD.HasRows) //SI TIENE FILAS
+            {
+                bool Find = false; // bandera
+                while (lectorBD.Read()) //mientras pueda leer, mostrar (leer)
+                {
+                    if (int.Parse(lectorBD[0].ToString()) == codigo)
+                    {
+
+                        //datosTabla += "-" + lectorBD[0]; //dato d la comlumna 0
+                        MessageBox.Show("Cliente Existente " + lectorBD[0], "Consulta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Find = true; // bandera
+                        break;
+                    }
+
+                }
+                if (Find == false)
+                {
+                    MessageBox.Show("NO Existente " + lectorBD[0], "Consulta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        public void actividadCliente(int codigo)
+        {
+            ConectarBD();
+
+            comandoBD = new OleDbCommand();
+            comandoBD.Connection = conexionBD;
+            comandoBD.CommandType = System.Data.CommandType.TableDirect;
+            comandoBD.CommandText = "Productos";
+
+            adaptadorBD = new OleDbDataAdapter(comandoBD);
+            adaptadorBD.Fill(objDataSet, "Productos");
+
+            DataTable dt = objDataSet.Tables["Productos"];
+
+            foreach (DataRow dr in dt.Rows)
+            {
+
+                if ((int)dr["Codigo del Producto"] == codigo)
+                {
+                    if ((bool)dr["ACTIVIDAD"] == false)
+                    {
+                        dr.BeginEdit();
+                        dr["ACTIVIDAD"] = true;
+                        dr.EndEdit();
+                        break;
+                    }
+                    else if ((bool)dr["ACTIVIDAD"] == true)
+                    {
+                        dr.BeginEdit();
+                        dr["ACTIVIDAD"] = false;
+                        dr.EndEdit();
+                        break;
+                    }
+                }
+            }
+
+            OleDbCommandBuilder cb = new OleDbCommandBuilder(adaptadorBD);
+
+            adaptadorBD.Update(objDataSet, "Productos");
+        }
+
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace PryTpLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BarraInferior = new System.Windows.Forms.StatusStrip();
+            this.lblEstadoConexion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dgvDatosProductos = new System.Windows.Forms.DataGridView();
+            this.btnGuardarProducto = new System.Windows.Forms.Button();
+            this.BarraInferior.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatosProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BarraInferior
+            // 
+            this.BarraInferior.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblEstadoConexion});
+            this.BarraInferior.Location = new System.Drawing.Point(0, 428);
+            this.BarraInferior.Name = "BarraInferior";
+            this.BarraInferior.Size = new System.Drawing.Size(520, 22);
+            this.BarraInferior.TabIndex = 0;
+            this.BarraInferior.Text = "statusStrip1";
+            // 
+            // lblEstadoConexion
+            // 
+            this.lblEstadoConexion.Name = "lblEstadoConexion";
+            this.lblEstadoConexion.Size = new System.Drawing.Size(12, 17);
+            this.lblEstadoConexion.Text = "-";
+            // 
+            // dgvDatosProductos
+            // 
+            this.dgvDatosProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatosProductos.Location = new System.Drawing.Point(15, 21);
+            this.dgvDatosProductos.Name = "dgvDatosProductos";
+            this.dgvDatosProductos.Size = new System.Drawing.Size(475, 305);
+            this.dgvDatosProductos.TabIndex = 1;
+            // 
+            // btnGuardarProducto
+            // 
+            this.btnGuardarProducto.Location = new System.Drawing.Point(296, 345);
+            this.btnGuardarProducto.Name = "btnGuardarProducto";
+            this.btnGuardarProducto.Size = new System.Drawing.Size(134, 57);
+            this.btnGuardarProducto.TabIndex = 2;
+            this.btnGuardarProducto.Text = "Guardar Producto";
+            this.btnGuardarProducto.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 450);
+            this.Controls.Add(this.btnGuardarProducto);
+            this.Controls.Add(this.dgvDatosProductos);
+            this.Controls.Add(this.BarraInferior);
+            this.Name = "Form1";
+            this.Text = "Trabajo Práctico Laboratorio 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.BarraInferior.ResumeLayout(false);
+            this.BarraInferior.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatosProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip BarraInferior;
+        private System.Windows.Forms.ToolStripStatusLabel lblEstadoConexion;
+        private System.Windows.Forms.DataGridView dgvDatosProductos;
+        private System.Windows.Forms.Button btnGuardarProducto;
+    }
+}
+
